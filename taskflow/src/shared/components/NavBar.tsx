c205bc9d95c3feeeadeb86f3a676e0d59e8fe2a3
Eyshield <samy.amaouche008@gmail.com>
2026-06-05 17:22:37 +0200

    feature: added a new components for navigating

---
+++ taskflow/src/shared/components/NavBar.tsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav>
+      <li>
+        <a href="/">DashBoard</a>
+        <a href="/">Projects</a>
+        <a href="/">Users</a>
+      </li>
+    </nav>
+  );
+}
+
+export default NavBar;
